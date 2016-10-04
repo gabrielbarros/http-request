53ef7f67914f6bfb39dd9239ec073ebfb7c4bc9e
Gabriel Santos <gabrielbarros@users.noreply.github.com>
2016-10-04 04:14:06 -0300

    Correção para o exemplo "location"

--- examples/location.php
+++ examples/location.php
@@ -11,5 +11,5 @@ $http->get('https://httpbin.org/redirect-to?url=https%3A%2F%2Fwww.google.com%2Fh
 header('Content-Type: text/plain');
 
 $headers = $http->responseHeaders;
-echo $headers['location'];
+echo $headers['Location'];
 
